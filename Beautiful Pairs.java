e840e4e5559a99fea11544df4b2857f72133fabd
Rishika Pandagre <pandagrerishika0195@gmail.com>
2026-03-26 15:34:17 +0530

    -

---
+++ Beautiful Pairs.java
@@ -0,0 +1,71 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'beautifulPairs' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY A
+     *  2. INTEGER_ARRAY B
+     */
+
+    public static int beautifulPairs(List<Integer> A, List<Integer> B) {
+    // Write your code here
+        int n = A.size();
+        int matches = 0;
+        
+        HashMap<Integer , Integer> freqB = new HashMap<>();
+        for(int b : B){
+            freqB.put(b, freqB.getOrDefault(b, 0) + 1);
+        }
+        
+        for(int a : A){
+            if(freqB.getOrDefault(a,0) > 0){
+                matches++;
+                freqB.put(a , freqB.get(a)-1);
+            }
+        }
+        
+        if(matches < n) return matches+1;
+        else return matches-1;
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> A = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        List<Integer> B = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        int result = Result.beautifulPairs(A, B);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
