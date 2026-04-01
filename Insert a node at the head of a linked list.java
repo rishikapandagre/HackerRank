fdaf5364aa7ef7807db5b06dd09cfe45cc32282b
Rishika Pandagre <pandagrerishika0195@gmail.com>
2026-04-01 16:20:46 +0530

    Create Insert a node at the head of a linked list.java

---
+++ Insert a node at the head of a linked list.java
@@ -0,0 +1,8 @@
+static SinglyLinkedListNode insertNodeAtHead(SinglyLinkedListNode llist, int data) {
+        SinglyLinkedListNode newNode = new SinglyLinkedListNode(data);
+        newNode.next = llist;
+        llist = newNode;
+        
+        return llist;
+
+    }
